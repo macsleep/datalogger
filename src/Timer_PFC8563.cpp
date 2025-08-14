0199003f0f151c4c6f805477233be78df49a1e1a
Jan Schlieper <42387407+macsleep@users.noreply.github.com>
2025-08-14 12:19:44 +0200

    moved set/clear

--- src/Timer_PFC8563.cpp
+++ src/Timer_PFC8563.cpp
@@ -35,12 +35,12 @@ void Timer_PFC8563::enable(uint8_t value) {
 
     Wire.requestFrom(PCF8563_ADDRESS, 1);
     data = Wire.read();
-
-    Wire.beginTransmission(PCF8563_ADDRESS);
-    Wire.write(PCF8563_TIMER_CONTROL);
     bitSet(data, 0);            // TD 1/60 Hz
     bitSet(data, 1);            // TD 1/60 Hz
     bitSet(data, 7);            // TE
+
+    Wire.beginTransmission(PCF8563_ADDRESS);
+    Wire.write(PCF8563_TIMER_CONTROL);
     Wire.write(data);
     Wire.endTransmission();
 

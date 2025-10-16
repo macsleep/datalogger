7746f228d9ee7a8e33fda26c43d2212d3e5e581d
Jan Schlieper <42387407+macsleep@users.noreply.github.com>
2025-10-16 10:09:24 +0200

    basic layout

--- data/scripts/app.js
+++ data/scripts/app.js
@@ -7,9 +7,45 @@ new Vue({
 	el: '#app',
 
 	data() {
+		return {
+			authenticated: false,
+			firmwareVersion: "???",
+		}
+	},
+
+	mounted() {
+	},
+
+	beforeUnmount() {
+	},
+
+	created() {
+		this.getFirmware();
+	},
+
+	computed: {
 	},
 
 	methods: {
+		getFirmware() {
+			axios.get('/api/firmware')
+				.then(response => {
+					this.firmwareVersion = response.data.version;
+				})
+				.catch(error => {
+					console.log(error);
+				});
+		},
+
+		doLogin() {
+			axios.post('/api/login', null)
+				.then(response => {
+					this.authenticated = true;
+				})
+				.catch(error => {
+					this.authenticated = false;
+				});
+		},
+
 	},
 });
-
